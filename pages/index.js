18d1f908b5c2545ee82d178d1dd46fe20527b831
Mauricio Osmainchi da Rosa <5149932+ankardo@users.noreply.github.com>
2023-07-13 01:06:29 +0000

    adiciona os arquivos scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
-function Home(){
-    return <h1>Olha só gatinha, alterei esse arquivo com o neovim dentro do ubuntu que ta dentro do windows</h1>
+function Home() {
+  return (
+    <h1>
+      Olha só gatinha, alterei esse arquivo com o neovim dentro do ubuntu que ta
+      dentro do windows
+    </h1>
+  );
 }
 
 export default Home;
-
